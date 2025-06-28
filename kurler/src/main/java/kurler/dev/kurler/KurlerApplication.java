8e3b5d7c0c4ac48f3458dfb6660158469e5dbe43
kurlerprudent <osarkodie@st.ug.edu.gh>
2025-06-28 00:33:58 +0000

    My demo spring boot

---
+++ kurler/src/main/java/kurler/dev/kurler/KurlerApplication.java
@@ -0,0 +1,13 @@
+package kurler.dev.kurler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlerApplication.class, args);
+	}
+
+}
---
+++ kurler/src/test/java/kurler/dev/kurler/KurlerApplicationTests.java
@@ -0,0 +1,13 @@
+package kurler.dev.kurler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
