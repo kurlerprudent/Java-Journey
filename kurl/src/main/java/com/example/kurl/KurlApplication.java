9635c37ca1e4a17fb247dfa2295f03277ac4ff53
kurlerprudent <osarkodie@st.ug.edu.gh>
2025-07-19 18:12:25 +0000

    Spring boot

---
+++ kurl/src/main/java/com/example/kurl/KurlApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlApplication.class, args);
+	}
+
+}
